8ba39e9561cb996021b8acdd9a739371091ee6d6
unknown <lyudmila_ds@abv.bg>
2021-06-30 17:11:04 +0300

    Added exercise in First Steps in OOP

---
+++ First Steps in OOP/exercise/08_pokemon_battle/project/trainer.py
@@ -0,0 +1,27 @@
+
+class Trainer:
+    def __init__(self, name):
+        self.name = name
+        self.pokemons = []
+
+    def add_pokemon(self, pokemon):
+        if pokemon not in self.pokemons:
+            self.pokemons.append(pokemon)
+            return f"Caught {pokemon.pokemon_details()}"
+        return "This pokemon is already caught"
+
+    def release_pokemon(self, pokemon_name):
+        pokemon_names = [p.name for p in self.pokemons]
+        if pokemon_name in pokemon_names:
+            del self.pokemons[pokemon_name.index(pokemon_name)]
+            return f"You have released {pokemon_name}"
+        return "Pokemon is not caught"
+
+    def trainer_data(self):
+        result = f"Pokemon Trainer {self.name}\n"
+        result += f"Pokemon count {len(self.pokemons)}\n"
+        for pokemon in self.pokemons:
+            result += f"- {pokemon.pokemon_details()}\n"
+
+        return result
+
