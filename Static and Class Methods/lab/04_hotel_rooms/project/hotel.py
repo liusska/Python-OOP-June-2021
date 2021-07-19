901f8ce941f4f5b27af03fd2cb14cbb81fed3dc9
unknown <lyudmila_ds@abv.bg>
2021-07-19 17:47:29 +0300

    Added lab for Static and Class Methods

---
+++ Static and Class Methods/lab/04_hotel_rooms/project/hotel.py
@@ -0,0 +1,31 @@
+class Hotel:
+    def __init__(self, name):
+        self.name = name
+        self.rooms = []
+        self.guests = 0
+
+    @classmethod
+    def from_stars(cls, stars_count):
+        return cls(f"{stars_count} stars Hotel")
+
+    def add_room(self, room):
+        self.rooms.append(room)
+
+    def take_room(self, room_number, people):
+        room = [r for r in self.rooms if r.number == room_number][0]
+        self.guests += people
+        room.take_room(people)
+
+    def free_room(self, room_number):
+        room = [r for r in self.rooms if r.number == room_number][0]
+        self.guests -= room.guests
+        room.free_room()
+
+    def status(self):
+        free_rooms = [x for x in self.rooms if not x.is_taken]
+        taken_rooms = [x for x in self.rooms if x.is_taken]
+        return f"Hotel {self.name} has {self.guests} total guests\n" \
+               f"Free rooms: {', '.join([str(x.number) for x in free_rooms])}\n" \
+               f"Taken rooms: {', '.join([str(x.number) for x in taken_rooms])}"
+
+
